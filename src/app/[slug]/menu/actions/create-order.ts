80f6aa35c0b6a889f6ef64301cc4a2bb0b16d62a
ihuryferreira <ihuryferreira@icloud.com>
2025-03-16 22:48:54 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -2,6 +2,7 @@
 
 import { ConsumptionMethod } from "@prisma/client";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -61,5 +62,6 @@ export const createOrder = async (input: CreateOrderInput) => {
   // redirect(
   //   `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
   // );
+  redirect(`/${input.slug}/orders`);
   return order;
 };
