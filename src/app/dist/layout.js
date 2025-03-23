fd7bb3abdf08b2195904bf1ea9f7a03c826501cf
ihuryferreira <ihuryferreira@icloud.com>
2025-03-23 19:43:13 -0300

    chore: adjust import styles and organize code

--- src/app/dist/layout.js
+++ src/app/dist/layout.js
@@ -1,28 +1,36 @@
 "use strict";
-exports.__esModule = true;
-exports.metadata = void 0;
 import "./globals.css";
 
 import { Poppins } from "next/font/google";
 
-import { Toaster as sonner_1 } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner"; // Ajustado para não usar alias no import
 
 import { CartProvider } from "./[slug]/menu/contexts/cart";
-var poppins = Poppins({
+
+const poppins = Poppins({
     weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    subsets: ["latin"]
+    subsets: ["latin"],
 });
-exports.metadata = {
+
+export const metadata = {
     title: "Donalds Delivery",
-    description: "Aproveite a conveniência de receber seu pedido em casa com Donald's Delivery. Saboreie o melhor do Donald's sem sair do conforto do seu lar. Peça seu sanduíche preferido e escolha: comer na loja ou levar para casa. Fácil, rápido e delicioso."
+    description:
+        "Aproveite a conveniência de receber seu pedido em casa com Donald's Delivery. Saboreie o melhor do Donald's sem sair do conforto do seu lar. Peça seu sanduíche preferido e escolha: comer na loja ou levar para casa. Fácil, rápido e delicioso.",
 };
-function RootLayout(_a) {
-    var children = _a.children;
-    return (React.createElement("html", { lang: "pt-br" },
-        React.createElement("head", null,
-            React.createElement("meta", { name: "viewport", content: "width=device-width, initial-scale=1.0" })),
-        React.createElement("body", { className: poppins.className + " antialiased" },
-            React.createElement(CartProvider, null, children),
-            React.createElement(sonner_1.Toaster, null))));
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="pt-br">
+            <head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </head>
+            <body className={`${poppins.className} antialiased`}>
+                <CartProvider>{children}</CartProvider>
+                <Toaster />
+            </body>
+        </html>
+    );
 }
-exports["default"] = RootLayout;
